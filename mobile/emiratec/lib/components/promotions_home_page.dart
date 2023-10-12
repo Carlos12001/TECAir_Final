6f149f3267381cc4ae8ffe7fb0c5ca1f56a1ac06
Ignacio Calderón <53590021+Apollo13-0@users.noreply.github.com>
2023-10-12 11:33:31 -0600

    promotions listView

---
+++ mobile/emiratec/lib/components/promotions_home_page.dart
@@ -0,0 +1,106 @@
+import 'package:emiratec/objects/promotion.dart';
+import 'package:flutter/material.dart';
+
+
+Expanded promotionsHomePage(List<promotion>? promotionList) {
+  return Expanded(
+    flex: 4,
+    child: FutureBuilder<List<promotion>>(
+      future:
+          getPromotionList(promotionList), // Aquí se supone que tendrías una función que retorna Future<List<Movie>>
+      builder: (BuildContext context, AsyncSnapshot<List<promotion>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+              child:
+                  CircularProgressIndicator()); // Muestra un indicador de progreso mientras se espera.
+        } else if (snapshot.hasError) {
+          return Text(
+              'Error: ${snapshot.error}'); // Muestra un mensaje de error si algo sale mal.
+        } else {
+          List<promotion>? promotions = snapshot.data;
+
+          return ListView.separated(
+            padding: const EdgeInsets.all(8),
+            itemCount: promotions!.length,
+            itemBuilder: (context, index) {
+              promotion currentPromotion = promotions[index];
+
+              return InkWell(
+                // Lógica para navegar a la nueva página
+                // onTap: () {
+                //   // Lógica para navegar a la nueva página
+                //   Navigator.push(
+                //     context,
+                //     MaterialPageRoute(
+                //       builder: (context) => MovieDetails(title: "Horarios",
+                //           pelicula:
+                //               peliculaActual), // Suponiendo que tienes una página llamada DetallePelicula
+                //     ),
+                //   );
+                // },
+                child: SizedBox(
+                  height: 150,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(
+                            width: 100,
+                            height: 120,
+                            child: FittedBox(
+                                fit: BoxFit.fill,
+                                child: Image.network(currentPromotion.imgPath)),
+                          ),
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment:CrossAxisAlignment.end,
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Text(
+                                  "Inicion de promoción: ${currentPromotion.startDate}",
+                                  overflow: TextOverflow.clip,
+                                  maxLines: 3,
+                                  style: const TextStyle(color: Color(0xFFfdfcfc)),
+                                ),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "Fin de promoción: ${currentPromotion..endDate}",
+                                      overflow: TextOverflow.fade,
+                                      maxLines: 2,
+                                      style: const TextStyle(color: Color(0xFFfdfcfc)),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) =>
+                const Divider(),
+          );
+        }
+      },
+    ),
+  );
+}
+
+
+Future<List<promotion>> getPromotionList(List<promotion>? promotionsList) async {
+  if(promotionsList != null){
+    return promotionsList;
+  } else{
+    return [];
+  }
+  
+}
