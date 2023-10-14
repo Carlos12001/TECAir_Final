c35d4a94f0f39b9f87e93cb77568ce0374ef7640
Carlos12001 <carlos.andres12001@gmail.com>
2023-10-14 13:30:40 -0600

    feat: add routing sing in

--- web/TECAir_Reservations/src/app/app-routing.module.ts
+++ web/TECAir_Reservations/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home' },
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'register', component: HomeComponent },
   { path: 'search-flights', component: HomeComponent },
   { path: 'seat-selection', component: HomeComponent },
   { path: 'checkout', component: HomeComponent },
