00381bac098977faf6ca6eb181c948df1598f27d
Carlos12001 <carlos.andres12001@gmail.com>
2023-10-16 12:04:26 -0600

    feat: add seats

--- web/TECAir_Reservations/src/app/models/seat.model.ts
+++ web/TECAir_Reservations/src/app/models/seat.model.ts
@@ -1,13 +1,153 @@
 export interface Seat {
   Snumber: string;
-  Sclass: string;
-  Pemail: string;
+  Sclass: 'Ejecutiva' | 'Turista' | '';
+  Pemail?: string;
   Pid: string;
 }
 
-export const seat = {
-  Snumber: '',
-  Sclass: '',
-  Pemail: '',
-  Pid: '',
-};
+export const seats: Seat[] = [
+  {
+    Snumber: 'A00',
+    Sclass: 'Ejecutiva',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A01',
+    Sclass: 'Ejecutiva',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A02',
+    Sclass: 'Ejecutiva',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A03',
+    Sclass: 'Ejecutiva',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A04',
+    Sclass: 'Ejecutiva',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A05',
+    Sclass: 'Ejecutiva',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A06',
+    Sclass: 'Ejecutiva',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A07',
+    Sclass: 'Turista',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A08',
+    Sclass: 'Turista',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A09',
+    Sclass: 'Turista',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A10',
+    Sclass: 'Turista',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A11',
+    Sclass: 'Turista',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A12',
+    Sclass: 'Turista',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A13',
+    Sclass: 'Turista',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A14',
+    Sclass: 'Turista',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A15',
+    Sclass: 'Turista',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A16',
+    Sclass: 'Turista',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A17',
+    Sclass: 'Turista',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A18',
+    Sclass: 'Turista',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A19',
+    Sclass: 'Turista',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A20',
+    Sclass: 'Turista',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A21',
+    Sclass: 'Turista',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A22',
+    Sclass: 'Turista',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+  {
+    Snumber: 'A23',
+    Sclass: 'Turista',
+    Pemail: '',
+    Pid: 'A0023',
+  },
+];
