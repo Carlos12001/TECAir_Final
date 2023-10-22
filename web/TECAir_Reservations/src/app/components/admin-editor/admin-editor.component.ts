ab19caf564c44362526cf53591926faf540fd5e5
Felipe Vargas <68176209+Felipe-V13@users.noreply.github.com>
2023-10-22 15:56:58 -0600

    feat update
    Co-authored-by: Carlos12001 <Carlos12001@users.noreply.github.com>

--- web/TECAir_Reservations/src/app/components/admin-editor/admin-editor.component.ts
+++ web/TECAir_Reservations/src/app/components/admin-editor/admin-editor.component.ts
@@ -1,7 +1,7 @@
-/*import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Flight } from '../../models/flight.model';
-import { FlightService } from '../../models/flight.model'; // Importando el supuesto servicio
+import { FormControl, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-editor',
@@ -14,183 +14,126 @@ export class AdminEditorComponent implements OnInit {
   flights: Flight[] = [];
   selectedFlight: Flight | null = null;
   originalFlight: Flight | null = null;
+  priceControl = new FormControl();
 
-  constructor(private router: Router, private flightService: FlightService) {}
-
-  ngOnInit(): void {}
-
-  deepCopy(obj: any): any {
-    if (obj === null || typeof obj !== 'object') {
-      return obj;
-    }
-    const copy: Record<string, any> = Array.isArray(obj) ? [] : {};
-    for (const key in obj) {
-      if (Object.prototype.hasOwnProperty.call(obj, key)) {
-        copy[key] = this.deepCopy(obj[key]);
-      }
-    }
-    return copy;
-  }
-
-  clearData() {
-    this.flights = [];
-    this.selectedFlight = null;
-    this.originalFlight = null;
-  }
-
-  trackByFunction(index: number, item: any): number {
-    return index;
-  }
-
-  setSelectedEntity(entity: string) {
-    this.clearData();
-    this.selectedEntity = entity;
-    if (entity === 'flights') {
-      this.loadFlights();
-    }
-  }
-
-  loadFlights() {
-    this.flightService.getFlights().subscribe({
-      next: (data: Flight[]) => {
-        this.flights = data;
-        console.log('Vuelos cargados exitosamente');
-      },
-      error: (err: any) => {
-        console.error('Error al cargar los vuelos:', err);
-      },
-      complete: () => {
-        console.log('La carga de los vuelos se ha completado.');
-      },
-    });
-  }
-
-  selectFlight(flight: Flight) {
-    this.selectedFlight = this.deepCopy(flight);
-    this.originalFlight = flight;
-  }
-
-  submitFlight() {
+  ngOnInit() {
     if (this.selectedFlight) {
-      this.flightService.updateFlight(this.selectedFlight).subscribe({
-        next: (response: any) => {
-          console.log('Vuelo actualizado exitosamente:', response);
-          // Actualizaciones adicionales que quieras hacer tras actualizar el vuelo
-        },
-        error: (err: any) => {
-          console.error('Error al actualizar el vuelo:', err);
-        },
-        complete: () => {
-          console.log('Actualización de vuelo completada.');
-        },
-      });
+      this.priceControl.setValue(this.selectedFlight.price);
     }
   }
 
-  deleteFlight() {
-    if (this.selectedFlight && this.selectedFlight.fnumber) {
-      this.flightService.deleteFlight(this.selectedFlight.fnumber).subscribe({
-        next: (response: any) => {
-          console.log('Vuelo eliminado exitosamente:', response);
-          this.loadFlights(); // Recargamos los vuelos tras eliminar uno
-          this.selectedFlight = null;
-        },
-        error: (err: any) => {
-          console.error('Error al eliminar el vuelo:', err);
-        },
-        complete: () => {
-          console.log('Eliminación de vuelo completada.');
-        },
-      });
+  sumitFlight() {}
+
+  deleteFlight() {}
+
+  submitFlight() {}
+
+  updatePrice(event: any): void {
+    if (this.selectedFlight) {
+      this.selectedFlight.price = Number(event.target.value);
     }
   }
-
-  addNewFlight() {
-    this.originalFlight = null;
-    this.selectedFlight = {
-      fnumber: 0,
-      ffrom: 0,
-      fto: 0,
-      price: 0,
-      fdate: '',
-      fstate: false,
-      pid: '',
-    };
-  }
 }
-*/
 
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Flight } from '../../models/flight.model';
-// Suponiendo que Airport es necesario en tu código, aunque no lo veo utilizado en el fragmento que has proporcionado.
-import { Airport } from '../../models/airport.model';
-// Asegúrate de que la ruta sea la correcta
-import { FlightService } from '../../models/flight.model';
+// constructor(private router: Router, private flightService: FlightService) {}
 
-@Component({
-  selector: 'app-admin-editor',
-  templateUrl: './admin-editor.component.html',
-  styleUrls: ['./admin-editor.component.css'],
-})
-export class AdminEditorComponent implements OnInit {
-  // Variables que posiblemente necesites en base al HTML
+// ngOnInit(): void {}
 
-  entities: any[] = [
-    // Puedes rellenar esta parte con los datos de tus entidades
-  ];
-  selectedEntity: string = '';
+// deepCopy(obj: any): any {
+//   if (obj === null || typeof obj !== 'object') {
+//     return obj;
+//   }
+//   const copy: Record<string, any> = Array.isArray(obj) ? [] : {};
+//   for (const key in obj) {
+//     if (Object.prototype.hasOwnProperty.call(obj, key)) {
+//       copy[key] = this.deepCopy(obj[key]);
+//     }
+//   }
+//   return copy;
+// }
 
-  selectedFlight: Flight = {
-    fnumber: 0,
-    ffrom: 0,
-    fto: 0,
-    price: 0,
-    fdate: '',
-    fstate: false,
-    pid: '',
-  };
+// clearData() {
+//   this.flights = [];
+//   this.selectedFlight = null;
+//   this.originalFlight = null;
+// }
 
-  constructor(private router: Router, private flightService: FlightService) {}
+// trackByFunction(index: number, item: any): number {
+//   return index;
+// }
 
-  ngOnInit(): void {
-    // Aquí puedes cargar los datos iniciales si es necesario
-  }
+// setSelectedEntity(entity: string) {
+//   this.clearData();
+//   this.selectedEntity = entity;
+//   if (entity === 'flights') {
+//     this.loadFlights();
+//   }
+// }
 
-  setSelectedEntity(entityValue: string): void {
-    this.selectedEntity = entityValue;
-    // Cargar datos relacionados con la entidad si es necesario
-  }
+// loadFlights() {
+//   this.flightService.getFlights().subscribe({
+//     next: (data: Flight[]) => {
+//       this.flights = data;
+//       console.log('Vuelos cargados exitosamente');
+//     },
+//     error: (err: any) => {
+//       console.error('Error al cargar los vuelos:', err);
+//     },
+//     complete: () => {
+//       console.log('La carga de los vuelos se ha completado.');
+//     },
+//   });
+// }
 
-  submitFlight(): void {
-    // Captura la información del formulario
-    const flightData = {
-      price: this.selectedFlight.price,
-      departureDate: (
-        document.getElementById('departureDate') as HTMLInputElement
-      ).value,
-      arrivalDate: (document.getElementById('arrivalDate') as HTMLInputElement)
-        .value,
-      dropdown1: (
-        document.querySelector('.row select:nth-child(1)') as HTMLSelectElement
-      ).value,
-      dropdown2: (
-        document.querySelector('.row select:nth-child(2)') as HTMLSelectElement
-      ).value,
-      dropdown3: (
-        document.querySelector('.row select:nth-child(3)') as HTMLSelectElement
-      ).value,
-    };
+// selectFlight(flight: Flight) {
+//   this.selectedFlight = this.deepCopy(flight);
+//   this.originalFlight = flight;
+// }
 
-    // Muestra la información en la consola
-    console.log(flightData);
+// submitFlight() {
+//   if (this.selectedFlight) {
+//     this.flightService.updateFlight(this.selectedFlight).subscribe({
+//       next: (response: any) => {
+//         console.log('Vuelo actualizado exitosamente:', response);
+//         // Actualizaciones adicionales que quieras hacer tras actualizar el vuelo
+//       },
+//       error: (err: any) => {
+//         console.error('Error al actualizar el vuelo:', err);
+//       },
+//       complete: () => {
+//         console.log('Actualización de vuelo completada.');
+//       },
+//     });
+//   }
+// }
 
-    // Implementa la lógica para enviar los datos del vuelo
-    // Por ejemplo, usando el servicio 'flightService'
-  }
+// deleteFlight() {
+//   if (this.selectedFlight && this.selectedFlight.fnumber) {
+//     this.flightService.deleteFlight(this.selectedFlight.fnumber).subscribe({
+//       next: (response: any) => {
+//         console.log('Vuelo eliminado exitosamente:', response);
+//         this.loadFlights(); // Recargamos los vuelos tras eliminar uno
+//         this.selectedFlight = null;
+//       },
+//       error: (err: any) => {
+//         console.error('Error al eliminar el vuelo:', err);
+//       },
+//       complete: () => {
+//         console.log('Eliminación de vuelo completada.');
+//       },
+//     });
+//   }
+// }
 
-  deleteFlight(): void {
-    // Implementa la lógica para eliminar un vuelo
-    // Por ejemplo, usando el servicio 'flightService'
-  }
-}
+// addNewFlight() {
+//   this.originalFlight = null;
+//   this.selectedFlight = {
+//     fnumber: 0,
+//     ffrom: 0,
+//     fto: 0,
+//     price: 0,
+//     fdate: '',
+//     fstate: false,
+//     pid: '',
+//   };
+// }
